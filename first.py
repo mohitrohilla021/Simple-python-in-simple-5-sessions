98171838d9707e394d0b54b2c36cbd70881c5d61
MOHIT KUMAR ROHILLA <49117131+mohitrohilla021@users.noreply.github.com>
2020-08-20 12:30:00 +0000

    Add files via upload

---
+++ first.py
@@ -0,0 +1,6 @@
+#Assignment to 1st chapter on the course
+#Use of the print statement
+print("Hello atom, this is Mohit Rohilla, Ready to play")
+#Just for fun lets make mohit be a string to show its meaning Use
+mohit="champion"
+print("I am a",mohit," !!!")
